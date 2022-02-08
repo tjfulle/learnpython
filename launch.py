fd90d747d75d1f45e6ef9c833b70a6bef0e314f0
Tim Fuller <tjfulle@sandia.gov>
2022-02-07 23:06:01 -0700

    updating to python 3

--- launch.py
+++ launch.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 import os
-import sys
 import signal
 from subprocess import Popen
-from argparse import ArgumentParser
 from os.path import dirname, realpath, join
 
 D = dirname(realpath(__file__))
-IPY_D = join(D, 'Python-2.7')
-
 env = dict(os.environ)
-env['JUPYTER_CONFIG_DIR'] = IPY_D
-env['IPYTHONDIR'] = IPY_D
-command = 'ipython notebook'
+command = 'jupyter lab'
 kwds = {'env': env, 'shell': True}
 try:
     kwds['preexec_fn'] = os.setsid
